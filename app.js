7ba3f8fd629dfc66c828ac9a66d6aada7e9dfc59
bashir-ahmed <babashir8811@gmail.com>
2021-02-17 21:57:30 +0600

    To Do App

---
+++ app.js
@@ -0,0 +1,45 @@
+const addBtn = document.querySelector(".plus");
+const input = document.querySelector(".todo-input");
+const todolist = document.querySelector(".todolist");
+
+addBtn.addEventListener("click", (addTodo) => {
+	addTodo.preventDefault();
+	if (input.value === "") {
+		alert("plz write something");
+	} else {
+		const div = document.createElement("div");
+		div.classList.add("todo");
+
+		// create new Li element element
+		const li = document.createElement("li");
+		li.classList.add("todo-items");
+		li.innerText = input.value;
+		div.appendChild(li);
+
+		// create new check box
+		const completeBtn = document.createElement("button");
+		completeBtn.classList.add("check");
+		completeBtn.innerHTML = '<i class="far fa-check-circle"></i>';
+		div.appendChild(completeBtn);
+		completeBtn.addEventListener("click", () => {
+			li.classList.add("checked");
+			alert("Are you sure that your task is completed?");
+		});
+
+		// create new trash button for delete it terget li(.todo-items)
+		const trashBtn = document.createElement("button");
+		trashBtn.classList.add("trash");
+		trashBtn.innerHTML = '<i class="far fa-trash-alt"></i>';
+		div.appendChild(trashBtn);
+
+		// grab the trash btn for delete Li
+		trashBtn.addEventListener("click", () => {
+			li.parentElement.remove();
+			alert("Are you sure to delete your task ?")
+		});
+
+		todolist.appendChild(div);
+
+		input.value = "";
+	}
+});
